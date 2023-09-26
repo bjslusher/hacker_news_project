234c79d24915d2bd8ac47b2f57e4157d9fee1ea6
bjslusher <brian.slusher29@gmail.com>
2023-09-25 20:57:10 -0700

    it's alive

--- scrape.py
+++ scrape.py
@@ -1,5 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res.text)
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+links = soup.select('.titleline')
+links2 = soup2.select('.titleline')
+subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+large_links = links + links2
+large_subtext = subtext + subtext2
+
+
+def sort_stories_by_vote(hnlist):
+    sorted_list = sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+    return sorted_list
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(large_links):
+        title = item.getText()
+        href = item.a.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+
+    return sort_stories_by_vote(hn)
+
+
+pprint.pprint(create_custom_hn(large_links, large_subtext))
